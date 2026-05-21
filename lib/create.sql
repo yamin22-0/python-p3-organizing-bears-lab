eb86f2de63e7fb3105ad1365a2e37b7bcaf0ace6
binyamin <binyaminahmed594@gmail.com>
2026-05-21 08:05:01 +0300

    done

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id integer PRIMARY KEY,
+  name text,
+  age integer,
+  sex text,
+  color text,
+  temperament text,
+  alive boolean
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', 20, 'M', 'brown', 'calm', 1),
+  ('Rowdy', 10, 'M', 'black', 'aggressive', 1),
+  ('Tabitha', 6, 'F', 'brown', 'calm', 1),
+  ('Sergeant Brown', 8, 'M', 'grizzly', 'aggressive', 1),
+  ('Melissa', 13, 'F', 'black', 'calm', 1),
+  ('Grinch', 2, 'M', 'white', 'aggressive', 1),
+  ('Wendy', 6, 'F', 'brown', 'calm', 1),
+  (NULL, 4, 'M', 'black', 'calm', 0);
